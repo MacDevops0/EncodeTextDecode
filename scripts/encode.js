91f22038c2ca3aa763ce738615f60d0d39e56a74
macdevops0 <macdevops0@gmail.com>
2024-03-14 14:45:50 -0300

    Alteração no CSS file : apagando os campos de entrada e saída

--- scripts/encode.js
+++ scripts/encode.js
@@ -7,28 +7,28 @@ let isEncoded = false;
 codeBtn.addEventListener('click', () => {
   const text = inputText.value;
   if (isEncoded) {
-    outputText.value = decodeText(text);
+    inputText.value = decodeText(text);
+    outputText.value = '';
     codeBtn.textContent = 'Codificar';
     isEncoded = false;
   } else {
     outputText.value = encodeText(text);
+    inputText.value = '';
     codeBtn.textContent = 'Descodificar';
     isEncoded = true;
   }
 });
 
 function encodeText(text) {
-  const shift = 3; // Deslocamento de 3 posições no alfabeto
+  const shift = 3;
   let encoded = '';
   for (let i = 0; i < text.length; i++) {
     let char = text[i];
     if (char.match(/[a-z]/i)) {
       let code = text.charCodeAt(i);
       if (code >= 65 && code <= 90) {
-        // Letras maiúsculas
         char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
       } else if (code >= 97 && code <= 122) {
-        // Letras minúsculas
         char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
       }
     }
@@ -38,17 +38,15 @@ function encodeText(text) {
 }
 
 function decodeText(text) {
-  const shift = 3; // Deslocamento de 3 posições no alfabeto
+  const shift = 3;
   let decoded = '';
   for (let i = 0; i < text.length; i++) {
     let char = text[i];
     if (char.match(/[a-z]/i)) {
       let code = text.charCodeAt(i);
       if (code >= 65 && code <= 90) {
-        // Letras maiúsculas
         char = String.fromCharCode(((code - 65 - shift + 26) % 26) + 65);
       } else if (code >= 97 && code <= 122) {
-        // Letras minúsculas
         char = String.fromCharCode(((code - 97 - shift + 26) % 26) + 97);
       }
     }
